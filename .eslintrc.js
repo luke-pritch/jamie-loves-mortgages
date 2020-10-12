c4129eaba9c56198355664f6fabc72672dfbcbb9
Luke Pritchard <luke-pritch@users.noreply.github.com>
2020-10-12 01:25:50 -0400

    Fixing build errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,5 +13,7 @@ module.exports = {
     ],
     rules: {
         "@typescript-eslint/no-explicit-any": "off",
+        "@typescript-eslint/no-use-before-define": "off",
+        "@typescript-eslint/no-unused-vars": "off",
     },
 };
